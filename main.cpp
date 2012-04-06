13f29f96d73cdc3e028b0982f31f988523a371e0
Thomas RICHARD <thomas.richard.pro@gmail.com>
2012-04-06 23:20:45 +0200

    Simple test function.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <QApplication>
+#include <iostream>
+#include "tifile.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    if(argc != 2) {
+        cerr << argv[0] << " expects one argument." << endl;
+        return EXIT_FAILURE;
+    }
 
-    return app.exec();
+    try {
+        TiFile file(argv[1]);
+    } catch(exception const& e) {
+        cerr << e.what() << endl;
+        return EXIT_FAILURE;
+    };
+
+    cout        << file.calc_model()          << endl
+         << hex << file.signature1()          << endl
+                << file.default_folder_name() << endl
+                << file.comment()             << endl
+         << hex << file.entries_number()      << endl
+         << hex << file.file_size()           << endl
+         << hex << file.signature2()          << endl;
+
+    return EXIT_SUCCESS;
+//    QApplication app(argc, argv);
+
+//    return app.exec();
 }
