0f287f4e3389dd024d4ec6e539d6c18d06e76aab
Thomas RICHARD <thomas.richard.pro@gmail.com>
2012-04-06 22:21:12 +0200

    TiFile constructor should be ok (needs debugging). Added getters. Added 1 to char* attributes length to make them standard, zero- Variable entries are sorted according to the corresponding data offset to allow linear reading of the file. Variable type management and call to corresponding type constructor is done.

--- tifile.cpp
+++ tifile.cpp
@@ -1,62 +1,125 @@
 #include "tifile.h"
 
-TiFile::TiFile(const QString file_path)
+TiFile::TiFile(QString const file_path) : m_file_path(file_path)
 {
-//    int offset = 0;
     QFile ti_file(file_path);
     if(!ti_file.open(QIODevice::ReadOnly)) {
         return;
     }
 
-    QDataStream header_reader(&ti_file);
+    QDataStream reader(&ti_file);
     // HEADER
-    header_reader.readRawData(m_calc_model, 8);
+    reader.readRawData(m_calc_model, 8);
+    m_calc_model[8] = '\0';
 
-    header_reader.setByteOrder(QDataStream::LittleEndian);
-    header_reader >> m_signature1;
+    reader.setByteOrder(QDataStream::LittleEndian);
+    reader >> m_signature1;
 
-    header_reader.setByteOrder(QDataStream::BigEndian);
-    header_reader.readRawData(m_default_folder_name, 8);
-    header_reader.readRawData(m_comment, 40);
+    reader.setByteOrder(QDataStream::BigEndian);
+    reader.readRawData(m_default_folder_name, 8);
+    m_default_folder_name[8] = '\0';
+    reader.readRawData(m_comment, 40);
+    m_comment[40] = '\0';
 
-    header_reader.setByteOrder(QDataStream::LittleEndian);
-    header_reader >> m_entries_number;
-
-//    offset += 60;
+    reader.setByteOrder(QDataStream::LittleEndian);
+    reader >> m_entries_number;
 
     // VARIABLE TABLE
     for(int i=0; i<m_entries_number; i++) {
         qint32 offset;
-        char name[8];
+        char name[9];
         qint8 type_id;
         qint8 attribute;
         qint16 var_number;
 
-        header_reader >> offset;
+        reader >> offset;
 
-        header_reader.setByteOrder(QDataStream::BigEndian);
-        header_reader.readRawData(name, 8);
+        reader.setByteOrder(QDataStream::BigEndian);
+        reader.readRawData(name, 8);
+        reader[8] = '\0';
 
-        header_reader.setByteOrder(QDataStream::LittleEndian);
-        header_reader >> type_id;
-        header_reader >> attribute;
-        header_reader >> var_number;
+        reader.setByteOrder(QDataStream::LittleEndian);
+        reader >> type_id;
+        reader >> attribute;
+        reader >> var_number;
 
         TiVarEntry *entry = new TiVarEntry(offset, name, type_id, attribute, var_number);
-        if(!entry->isFolder()) {
-//            QDataStream data_reader(&ti_file);
-//            data_reader.skipRawData(offset);
-//            m_entries.last()->setVariable(new TiVar());
-        }
         m_entries.append(entry);
-//TODO: sorting entries by variable offset with folders first
-//        offset += 16;
     }
+    qSort(m_entries);
 
-    header_reader.setByteOrder(QDataStream::LittleEndian);
-    header_reader >> m_file_size;
-    header_reader >> m_signature2;
+    reader.setByteOrder(QDataStream::LittleEndian);
+    reader >> m_file_size;
+    reader >> m_signature2;
 
-//    offset += 6;
+    reader.setByteOrder(QDataStream::BigEndian);
+    for(QList<TiVarEntry*>::iterator it=m_entries.begin(); it!=m_entries.end(); it++) {
+        if(it->isFolder()) {
+            continue;
+        }
+
+        qint32 length;
+        if(it+1 != m_entries.end()) {
+            length = (it+1)->offset() - it->offset();
+        } else {
+            length = m_file_size - it->offset();
+        }
+        char *data = new char[length];
+        reader.readRawData(data, length);
+        TiVar *variable = NULL;
+
+        switch(it->type_id) {
+            case TiVarEntry::Expression:{
+//                    variable = new TiExpressionVar();
+                break;
+            }
+            case TiVarEntry::List:{
+//                    variable = new TiListVar();
+                break;
+            }
+            case TiVarEntry::Matrix:{
+//                    variable = new TiMatrixVar();
+                break;
+            }
+            case TiVarEntry::Data:{
+//                    variable = new TiDataVar();
+                break;
+            }
+            case TiVarEntry::Text:{
+//                    variable = new TiTextVar();
+                break;
+            }
+            case TiVarEntry::String:{
+                variable = new TiStringVar(data);
+                break;
+            }
+            case TiVarEntry::GDB:{
+//                    variable = new TiGDBVar();
+                break;
+            }
+            case TiVarEntry::Figure:{
+//                    variable = new TiFigureVar();
+                break;
+            }
+            case TiVarEntry::Picture:{
+//                    variable = new TiPictureVar();
+                break;
+            }
+            case TiVarEntry::Program:{
+//                    variable = new TiProgramVar();
+                break;
+            }
+            case TiVarEntry::Function:{
+//                    variable = new TiFunctionVar();
+                break;
+            }
+            case TiVarEntry::Macro:{
+//                    variable = new TiMacroVar();
+                break;
+            }
+        }
+        it->setVariable(variable);
+        delete[] data;
+    }
     ti_file.close();
 }
--- tifile.h
+++ tifile.h
@@ -1,24 +1,56 @@
+#ifndef TIFILE_H
+#define TIFILE_H
+
 #include <qdatastream.h>
 #include <qfile.h>
 #include <qlist.h>
 
-#include "tivar.h"
 #include "tivarentry.h"
-
-#ifndef TIFILE_H
-#define TIFILE_H
+#include "tivar.h"
+#include "tistringvar.h"
 
 class TiFile
 {
 public:
-    TiFile(const QString filepath);
+    TiFile(const QString file_path);
+
+    // GETTERS
+    inline char const *calc_model() const {
+        return m_calc_model;
+    }
+
+    inline qint16 const &signature1() const {
+        return m_signature1;
+    }
+
+    inline char const *default_folder_name() const {
+        return m_default_folder_name;
+    }
+
+    inline char const *comment() const {
+        return m_comment;
+    }
+
+    inline qint16 const &entries_number() const {
+        return m_entries_number;
+    }
+
+    inline qint32 const &file_size() const {
+        return m_file_size;
+    }
+
+    inline qint16 const &signature2() const {
+        return m_signature2;
+    }
 
 protected:
+    QString m_file_path;
+
     // HEADER
-    char    m_calc_model[8];            // 8-character signature. The signature is always "**TI89**".
+    char    m_calc_model[9];            // 8-character signature. The signature is always "**TI89**".
     qint16  m_signature1;               // 2-byte further signature. These two bytes always contain {01h, 00h} = {1, 0}.
-    char    m_default_folder_name[8];   // Default folder name (zero terminated unless 8 characters long).
-    char    m_comment[40];              // Comment. The comment is either zero-terminated or padded on the right with space characters.
+    char    m_default_folder_name[9];   // Default folder name (zero terminated unless 8 characters long).
+    char    m_comment[41];              // Comment. The comment is either zero-terminated or padded on the right with space characters.
     qint16  m_entries_number;           // Number of variable and folder entries in the variable table.
 
     // VARIABLE TABLE
