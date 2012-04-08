2b76f41c6c18f9a2c6a2690f85de49cd560c2619
Thomas RICHARD <thomas.richard.pro@gmail.com>
2012-04-08 12:34:53 +0200

    Updated TiStringVar prototypes to fit with TiVar ones. Added destructor and parse method. Renamed attributes.

--- tistringvar.cpp
+++ tistringvar.cpp
@@ -1,6 +1,33 @@
 #include "tistringvar.h"
 
-TiStringVar::TiStringVar(char const *data)
+TiStringVar::TiStringVar(char *raw_data, qint32 const &length, qint16 const &checksum) :
+    TiVar(raw_data, length, checksum)
 {
-    //TODO: Parse data to attributes
+
+}
+
+TiStringVar::~TiStringVar()
+{
+    delete[] m_data;
+}
+
+void TiStringVar::parse()
+{
+//TODO: error management / checking signatures
+    QBuffer buffer;
+    buffer.setData(m_raw_data, m_raw_data_length);
+    QDataStream reader(&buffer);
+
+    reader.setByteOrder(QDataStream::LittleEndian);
+    reader >> m_size; // should be m_raw_data_length - 2
+    reader >> m_signature1; // should be 0x00
+
+    m_data = new char[m_size-2];
+    reader.setByteOrder(QDataStream::BigEndian);
+    reader.readRawData(m_data, m_size-2);
+
+    reader.setByteOrder(QDataStream::LittleEndian);
+    reader >> m_signature2; // should be 0x2D
+
+    buffer.close();
 }
