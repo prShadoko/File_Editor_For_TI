bc7bf0ad1a65408c8a9b82d88b42b0909ffec4f5
Thomas RICHARD <thomas.richard.pro@gmail.com>
2012-04-18 18:26:08 +0200

    TiVarEntry variables type constants changed to an enum. Removed sorting methods.

--- tivarentry.h
+++ tivarentry.h
@@ -7,20 +7,22 @@
 class TiVarEntry
 {
 public:
-    static qint8 const Expression = 0x00;
-    static qint8 const List       = 0x04;
-    static qint8 const Matrix     = 0x06;
-    static qint8 const Data       = 0x0A;
-    static qint8 const Text       = 0x0B;
-    static qint8 const String     = 0x0C;
-    static qint8 const GDB        = 0x0D;
-    static qint8 const Figure     = 0x0E;
-    static qint8 const Picture    = 0x10;
-    static qint8 const Program    = 0x12;
-    static qint8 const Function   = 0x13;
-    static qint8 const Macro      = 0x14;
+    enum VarType {
+        Expression = 0x00,
+        List       = 0x04,
+        Matrix     = 0x06,
+        Data       = 0x0A,
+        Text       = 0x0B,
+        String     = 0x0C,
+        GDB        = 0x0D,
+        Figure     = 0x0E,
+        Picture    = 0x10,
+        Program    = 0x12,
+        Function   = 0x13,
+        Macro      = 0x14
+    };
 
-    TiVarEntry(qint32 const &offset, char const *name, qint8 const &type_id, qint8 const &attribute, qint16 const &var_number);
+    TiVarEntry(qint32 const &offset, char const *name, VarType &type_id, qint8 const &attribute, qint16 const &var_number);
     ~TiVarEntry();
     bool isFolder() const;
     // WARNING: Those may not have the expected signature
@@ -34,7 +36,7 @@ public:
     inline char const *name() const {
         return m_name;
     }
-    inline qint8 const &type_id() const {
+    inline VarType const &type_id() const {
         return m_type_id;
     }
     inline qint8 const &attribute() const {
@@ -55,7 +57,7 @@ protected:
     qint32 m_offset;        // Variable : Offset to the data for this variable from the beginning of the file.
                             // Folder : Offset to the data for the next variable from the beginning of the file.
     char m_name[9];         // Name of variable/folder (zero terminated if not 8 characters long).
-    qint8 m_type_id;        // Variable : Type ID of the variable.
+    VarType m_type_id;      // Variable : Type ID of the variable.
                             // Folder : This value is always 1Fh.
     qint8 m_attribute;      // Variable : Attribute (0: none, 1: locked, 2: archived)
                             // Folder : This value is always 0.
