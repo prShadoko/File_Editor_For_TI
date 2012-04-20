87615fefa8a90f89179073ef85c0a5161a361712
Thomas RICHARD <thomas.richard.pro@gmail.com>
2012-04-20 21:11:45 +0200

    Added getters.

--- titextline.h
+++ titextline.h
@@ -22,6 +22,17 @@ public:
     TiTextLine(char *data = NULL, LineEnd const eol = LineBreak, LineType const type = Normal);
     ~TiTextLine();
 
+    // GETTERS
+    inline LineType type() const {
+        return m_type;
+    }
+    inline char const *data() const {
+        return m_data;
+    }
+    inline LineEnd eol() const {
+        return m_eol;
+    }
+
 protected:
     LineType m_type;    // Line type: 0Ch=page break, 20h=normal, 43h=Command, 50h=PrintObj
     char *m_data;       // Characters, first to last. The string is not zero-terminated.
