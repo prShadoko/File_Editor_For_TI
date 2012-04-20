prShadoko/File_Editor_For_TI

commit 1db447883877f350e51d08612bf130e558472604

TiVar and TiStringVar structure modification. Added default constructors. Added check() method to check data integrity. TiVar :   Renamed m_raw_data_length attribute into m_size.   Changed m_size attribute type to qint16.   Added getter method for m_size attribute. TiStringVar :   Removed m_size attribute and its getter method (already in TiVar class).   Updated parse() method to fit with latest TiFile and TiVar changes.   Secured destructor (doesn't free an unallocated variable anymore).

tivar.cpp
@@ -1,23 +1,30 @@
 #include "tivar.h"
 
-TiVar::TiVar(char *raw_data, qint32 const &length, qint16 const &checksum) :
-    m_raw_data(raw_data),
-    m_raw_data_length(length),
-    m_checksum(checksum)
+TiVar::TiVar() :
+    m_raw_data(NULL),
+    m_size(0),
+    m_checksum(0)
 {
-    if(calc_checksum() != m_checksum) {
-        Excep(10, QString("Invalid checksum. File is corrupted."));
-    }
 }
 
-TiVar::~TiVar() {
-    delete[] m_raw_data;
+TiVar::TiVar(char *raw_data, qint32 const &size, qint16 const &checksum) :
+    m_raw_data(raw_data),
+    m_size(size),
+    m_checksum(checksum)
+{
+}
+
+TiVar::~TiVar()
+{
+    if (m_raw_data != NULL) {
+        delete[] m_raw_data;
+    }
 }
 
 qint16 TiVar::calc_checksum() const
 {
     qint16 checksum = 0;
-    for(qint32 i=0; i<m_raw_data_length; i++)
+    for(qint32 i=0; i<m_size; i++)
     {
         checksum += (unsigned char)m_raw_data[i];
     }

tistringvar.cpp
@@ -1,26 +1,32 @@
 #include "tistringvar.h"
 
-TiStringVar::TiStringVar(char *raw_data, qint32 const &length, qint16 const &checksum) :
-    TiVar(raw_data, length, checksum)
+TiStringVar::TiStringVar() :
+    TiVar(),
+    m_signature1(0x00),
+    m_data(NULL),
+    m_signature2(0x2D)
 {
+}
 
+TiStringVar::TiStringVar(char *raw_data, qint32 const &size, qint16 const &checksum) :
+    TiVar(raw_data, size, checksum)
+{
 }
 
 TiStringVar::~TiStringVar()
 {
-    delete[] m_data;
+    if(m_data != NULL) {
+        delete[] m_data;
+    }
 }
 
 void TiStringVar::parse()
 {
     QBuffer buffer;
-    buffer.setData(m_raw_data, m_raw_data_length);
+    buffer.setData(m_raw_data, m_size);
     buffer.open(QIODevice::ReadOnly);
     QDataStream reader(&buffer);
 
-    reader.setByteOrder(QDataStream::BigEndian);
-    reader.skipRawData(4);              // skip the first four 0x00 bytes
-    reader >> m_size;                   // size of the following data
     qint16 length = m_size - 2;         // size of the string itself
     reader >> m_signature1;             // should be 0x00
     m_data = new char[length];
@@ -29,7 +35,14 @@ void TiStringVar::parse()
 
     buffer.close();
 
+}
+
+void TiStringVar::check() throw(Excep)
+{
     if(m_signature1 != 0x00 || m_signature2 != 0x2D) {
         Excep(20, QString("Invalid signature encountered. File is corrupted.")).raise();
+    }
+    if(m_checksum != calc_checksum()) {
+        Excep(21, QString("Invalid checksum. File is corrupted.")).raise();
     }
 }

tistringvar.h
@@ -8,14 +8,13 @@
 class TiStringVar : public TiVar
 {
 public:
-    TiStringVar(char *raw_data, qint32 const &length, qint16 const &checksum);
+    TiStringVar();
+    TiStringVar(char *raw_data, qint32 const &size, qint16 const &checksum);
     ~TiStringVar();
     virtual void parse();
+    virtual void check() throw(Excep);
 
     // GETTERS
-    inline qint16 const &size() const {
-        return m_size;
-    }
     inline qint8 const &signature1() const {
         return m_signature1;
     }
@@ -27,7 +26,6 @@ public:
     }
 
 protected:
-    qint16 m_size;      // Number of bytes in this string minus two for this field.
     qint8 m_signature1; // This byte always has a value of 0.
     char *m_data;       // Characters, first to last. The string is zero-terminated.
     qint8 m_signature2; // This byte always has a value of 2Dh.

tivar.h
@@ -7,24 +7,29 @@
 class TiVar
 {
 public:
-    TiVar(char *raw_data, qint32 const &length, qint16 const &checksum);
+    TiVar();
+    TiVar(char *raw_data, qint32 const &size, qint16 const &checksum);
     virtual ~TiVar();
 
-    qint16 calc_checksum() const;
     virtual void parse() = 0;
+    virtual void check() throw(Excep) = 0;
+    qint16 calc_checksum() const;
 
     // GETTERS
     inline char const *raw_data() const {
         return m_raw_data;
+    }
+    inline qint16 const &size() const {
+        return m_size;
     }
     inline qint16 const &checksum() const {
         return m_checksum;
     }
 
 protected:
-    char *m_raw_data;           // Data section.
-    qint32 m_raw_data_length;   //
-    qint16 m_checksum;          // Checksum. This is the lower 16 bits of the sum of all bytes in the first four bytes and the variable data.
+    char *m_raw_data;       // The variable raw data
+    qint16 m_size;          // Size of variable data.
+    qint16 m_checksum;      // Checksum. This is the lower 16 bits of the sum of all bytes in the variable data.
 };
 
 #endif // TIVAR_H

commit e3d6f9872e43c6b65b436c8cb84fbfee210b6318

Corrected TiVar calc_checksum() method. Bug introduced in commit 1db447883877f350e51d08612bf130e558472604 (m_size bytes were not counted in the checksum anymore).

tivar.cpp
@@ -23,7 +23,9 @@ TiVar::~TiVar()
 
 qint16 TiVar::calc_checksum() const
 {
-    qint16 checksum = 0;
+    unsigned char size_bytes[2];
+    memcpy(size_bytes, reinterpret_cast<const unsigned char*>(&m_size), 2);
+    qint16 checksum = size_bytes[0] + size_bytes[1];
     for(qint32 i=0; i<m_size; i++)
     {
         checksum += (unsigned char)m_raw_data[i];